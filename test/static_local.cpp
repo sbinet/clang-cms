4c326872c8acf3bb0956094d84acd3713d03a53b
hauth <hauth@4525493e-7705-40b1-a816-d608a930855b>
2012-06-11 16:41:41 +0000

    fixed a bug. constructs like int const& val; int const* val; were detected as false positives.
    git-svn-id: https://svn.cern.ch/reps/cmscoperformance/projects/clang_cms@53 4525493e-7705-40b1-a816-d608a930855b

--- test/static_local.cpp
+++ test/static_local.cpp
@@ -7,7 +7,16 @@ public:
     void bar()
     {
         // will produce a warning by StaticLocalChecker
-        static int evilStaticLocal;
+        static int evilStaticLocal = 0;
+		static int & intRef = evilStaticLocal;
+		static int * intPtr = & evilStaticLocal;
+
+		// no warnings here
+		    static const int c_evilStaticLocal = 0;
+		static int const& c_intRef = evilStaticLocal;
+		static int const* c_intPtr = &evilStaticLocal;
+
+		static int const* const* c_intPtrPtr = &( c_intPtr);
     }
 };
 
